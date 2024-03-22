377ef1f1c1bdde8283ec632136c94dd608d83b8d
Otavio Goulart <otaviobgoulartt@gmail.com>
2024-03-21 22:35:30 -0300

    feat: model getlinkSchema

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 
-export const createLinkSchema = z.object({
+const createLinkSchema = z.object({
     code: z.string().min(3),
     url: z.string().url()
 })
+
+const getLinkSchema = z.object({
+    code: z.string().min(3)
+})
+
+export { createLinkSchema, getLinkSchema }
